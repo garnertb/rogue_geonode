08bab044c00cff1596e241c6f770bddd1b3a9dea
John Wileczek <john.wileczek@gmail.com>
2015-02-23 23:39:33 -0500

    Fixed some errors that were caught by jslint.

--- geoshape/firestation/static/firestation/firestation.js
+++ geoshape/firestation/static/firestation/firestation.js
@@ -8,12 +8,12 @@
     $interpolateProvider.endSymbol(']}');
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-  
+
   })
 
-  .controller('fireStationController', function($scope, $window, $http,$animate) {
-    
-    var thisFirestation = '/api/v1/firestations/'+config.id+'/';
+  .controller('fireStationController', function($scope, $window, $http, $animate) {
+
+    var thisFirestation = '/api/v1/firestations/' + config.id + '/';
     var options = {
         boxZoom: false,
         zoom: 15,
@@ -24,8 +24,8 @@
         doubleClickZoom: false,
         fullscreenControl: false
     };
-    
-    $scope.choices =        ['Engine',
+
+    $scope.choices = ['Engine',
                                 'Ladder/Truck/Aerial',
                                 'Quint',
                                 'Ambulance/ALS',
@@ -36,17 +36,17 @@
                                 'Chief',
                                 'Other'];
     $scope.forms = [];
-                     
-    var getUrl = '/api/v1/capabilities/?firestation='+config.id+'&format=json';
-    $http.get(getUrl).success(function(data){
-        for(var iForm = 0; iForm < data.meta.total_count; iForm++) {
+
+    var getUrl = '/api/v1/capabilities/?firestation=' + config.id + '&format=json';
+    $http.get(getUrl).success(function(data) {
+        for (var iForm = 0; iForm < data.meta.total_count; iForm++) {
             data.objects[iForm].name = data.objects[iForm].apparatus + data.objects[iForm].id;
         }
         $scope.forms = data.objects;
     });
-    
+
     $scope.map = L.map('map', options).setView(config.centroid, 13);
-    
+
     $scope.ClearForm = function(form) {
             form.apparatus = 'Engine';
             form.firefighter = 0;
@@ -59,7 +59,7 @@
             form.ems_supervisor = 0;
             form.chief = 0;
     };
-    
+
     $scope.AddForm = function() {
         var newForm = {'apparatus': 'Engine',
                         'chief_officer': 0,
@@ -74,30 +74,30 @@
                         'officer_paramedic': 0
                         };
         var postUrl = '/api/v1/capabilities/?format=json';
-        $http.post(postUrl, newForm).success(function(data,status,headers){
-                $http.get(headers('Location')+'?format=json').success(function(data){
+        $http.post(postUrl, newForm).success(function(data, status, headers) {
+                $http.get(headers('Location') + '?format=json').success(function(data) {
                 data.name = data.apparatus + data.id;
                 $scope.forms.push(data);
                 });
             $('.apparatus-tabs li:nth-last-child(0) a').tab('show');
         });
     };
-    
+
     $scope.UpdateForm = function(form) {
-            var updateUrl = '/api/v1/capabilities/'+form.id+'/?format=json';
-            $http.put(updateUrl,form).success(function(data){
+            var updateUrl = '/api/v1/capabilities/' + form.id + '/?format=json';
+            $http.put(updateUrl, form).success(function(data) {
                 form.name = form.apparatus + form.id;
             });
     };
-    
+
     $scope.DeleteForm = function(form) {
-        var deleteUrl = '/api/v1/capabilities/'+form.id+'/?format=json';
-        $http.delete(deleteUrl).success(function(data){
+        var deleteUrl = '/api/v1/capabilities/' + form.id + '/?format=json';
+        $http.delete(deleteUrl).success(function(data) {
             $scope.forms.splice($scope.forms.indexOf(form), 1);
         });
         $('.apparatus-tabs li:nth-last-child(2) a').tab('show');
     };
-   
+
    L.marker(config.centroid).addTo($scope.map);
     L.tileLayer('https://{s}.tiles.mapbox.com/v3/examples.map-i87786ca/{z}/{x}/{y}.png', {'attribution': '© Mapbox'})
         .addTo($scope.map);
