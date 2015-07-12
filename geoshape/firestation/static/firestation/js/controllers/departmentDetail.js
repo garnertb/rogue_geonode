dba144bbfd2818210cbb991a84ac94c78d268ecb
Tyler Garner <garnertb@gmail.com>
2015-07-12 09:33:12 -0400

    Make layers toggleable in the department page.  Fixes #24.

--- geoshape/firestation/static/firestation/js/controllers/departmentDetail.js
+++ geoshape/firestation/static/firestation/js/controllers/departmentDetail.js
@@ -10,14 +10,7 @@
           var headquartersIcon = L.FireCARESMarkers.headquartersmarker();
           var fitBoundsOptions = {};
           $scope.stations = [];
-
-          function onResize() {
-              var height = $(window).height();
-              $('.content-height').css('height', (height - $('.navbar').height()).toString() + 'px');
-            }
-
-            onResize();
-            $(window).resize(onResize);
+          var layersControl = L.control.layers().addTo(departmentMap);
 
           if (showStations) {
               FireStation.query({department: config.id}).$promise.then(function(data) {
@@ -33,7 +26,11 @@
                   }
 
                   var stationLayer = L.featureGroup(stationMarkers);
-                  stationLayer.addTo(departmentMap);
+
+                  // Uncomment to show Fire Stations by default
+                  // stationLayer.addTo(departmentMap);
+
+                  layersControl.addOverlay(stationLayer, 'Fire Stations');
 
                   if (config.geom === null) {
                     departmentMap.fitBounds(stationLayer.getBounds(), fitBoundsOptions);
@@ -42,7 +39,9 @@
           }
 
           if (config.centroid != null) {
-           L.marker(config.centroid, {icon: headquartersIcon}).addTo(departmentMap);
+           var headquarters = L.marker(config.centroid, {icon: headquartersIcon});
+           headquarters.addTo(departmentMap);
+           layersControl.addOverlay(headquarters, 'Headquarters Location');
           };
 
           if (config.geom != null) {
@@ -51,6 +50,7 @@
                                       return {color: '#0074D9', fillOpacity: .05, opacity:.8, weight:2};
                                   }
                               }).addTo(departmentMap);
+            layersControl.addOverlay(countyBoundary, 'Jurisdiction Boundary');
             departmentMap.fitBounds(countyBoundary.getBounds(), fitBoundsOptions);
           } else {
               departmentMap.setView(config.centroid, 13);
